7443c3b07fcc147280d2027f4f9deb88b2bf63b7
Aleksandr <a.v.agafonov@mail.ru>
2023-02-19 22:02:30 +0300

    Programm code for an array of three characters

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string[] array2;
+Console.WriteLine("Введите массив строк через пробел: ");
+string array1 = Console.ReadLine()!;
+array2 = array1.Split(' ');
+var result = new string[array2.Length];
+var realSize = 0;
+foreach (var value in array2)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(' ', result, 0, realSize));
+Console.ReadKey(true);
+
+
+
+
+
